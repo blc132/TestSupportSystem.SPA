cdb2a48e9635caef9478e0f864c5ae1a7977bb62
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-17 01:06:06 +0200

    Wstępne menu główne

---
+++ src/app/features/nav/NavBar.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { Menu, Container, Dropdown } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { NavLink, Link } from 'react-router-dom';
+import { RootStoreContext } from '../../stores/rootStore';
+
+const NavBar: React.FC = () => {
+    const rootStore = useContext(RootStoreContext);
+    const { user, logout } = rootStore.userStore;
+    return (
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header as={NavLink} exact to='/'>
+                    <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+                    SysEgz
+        </Menu.Item>
+                <Menu.Item name='Kursy' as={NavLink} to='/courses' />
+                {user && (
+                    <Menu.Item position='right'>
+                        <Dropdown pointing='top left' text={user.username}>
+                            <Dropdown.Menu>
+                                <Dropdown.Item
+                                    as={Link}
+                                    to={`/profile/${user.username}`}
+                                    text='Mój profil'
+                                    icon='user'
+                                />
+                                <Dropdown.Item onClick={logout} text='Wyloguj' icon='power' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                )}
+            </Container>
+        </Menu>
+    );
+};
+
+export default observer(NavBar);
