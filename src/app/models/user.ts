cdcb2ee6e8a9bf2621027e13643062b07eadab03
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-17 01:00:45 +0200

    Dodanie interfejsów związanych z User

---
+++ src/app/models/user.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    token: string;
+}
+
+export interface IUserFormValues {
+    email: string;
+    password: string;
+    firstName?: string;
+    lastName?: string;
+    username?: string;
+}
