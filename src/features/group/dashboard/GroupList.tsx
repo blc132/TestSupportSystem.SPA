5e4c6251a244f2c979d4d35beb10be1fa2c8ff58
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-22 01:01:19 +0200

    Poprawka widoku listy grup

--- src/features/group/dashboard/GroupList.tsx
+++ src/features/group/dashboard/GroupList.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, Fragment } from 'react';
-import { Item, Label, Card, Button, Image } from 'semantic-ui-react';
+import { Item, Label, Card, Button, Image, Segment } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../../../app/stores/rootStore';
 import { Link } from 'react-router-dom';
@@ -10,28 +10,35 @@ const GroupList: React.FC = () => {
     loadGroups();
 
     return (
-        <Card.Group>
-            {groups.map((group) => (
-                <Card>
-                    <Card.Content>
-                        <Card.Header as={Link} to={`/group/${group.id}`}>{group.name}</Card.Header>
-                        <Card.Meta>{group.course.name}</Card.Meta>
-                    </Card.Content>
-                    <Card.Content extra>
-                    <Card.Meta>Zarządzaj studentami</Card.Meta>
-                    <div className='ui two buttons'>
+        <Segment>
+            <Card>
+                <Card.Content>
+                    <Card.Header><h1>Grupy</h1></Card.Header>
+                </Card.Content>
+            </Card>
+            <Card.Group>
+                {groups.map((group) => (
+                    <Card>
+                        <Card.Content>
+                            <Card.Header as={Link} to={`/group/${group.id}`}>{group.name}</Card.Header>
+                            <Card.Meta>{group.course.name}</Card.Meta>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Card.Meta>Zarządzaj studentami</Card.Meta>
+                            <div className='ui two buttons'>
 
-          <Button color='green'>
-            Dodaj
+                                <Button color='green'>
+                                    Dodaj
           </Button>
-          <Button color='red'>
-            Usuń
+                                <Button color='red'>
+                                    Usuń
           </Button>
-        </div>
-        </Card.Content>
-                </Card>
-            ))}
-        </Card.Group>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                ))}
+            </Card.Group>
+        </Segment>
     );
 };
 
