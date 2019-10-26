a475eb3755b8cedf20dd923cd654105041b63cc0
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-26 19:44:16 +0200

    Dodanie navbarów w zależności od roli

--- src/features/nav/NavBar.tsx
+++ src/features/nav/NavBar.tsx
@@ -3,57 +3,31 @@ import { Menu, Container, Dropdown, MenuHeader } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import { NavLink, Link } from 'react-router-dom';
 import { RootStoreContext } from '../../app/stores/rootStore';
-import CourseForm from '../course/CourseForm';
-import GroupForm from '../group/GroupForm';
-import RegisterUserForm from '../user/RegisterUserForm';
+import NavBarAdmininstrator from './navroles/NavBarAdministrator'
+import NavBarMainLecturer from './navroles/NavBarMainLecturer'
+import NavBarLecturer from './navroles/NavBarLecturer'
+import NavBarStudent from './navroles/NavBarStudent'
 
 const NavBar: React.FC = () => {
     const rootStore = useContext(RootStoreContext);
     const { user, logout } = rootStore.userStore;
-    const { openModal } = rootStore.modalStore
     return (
         <Menu fixed='top' inverted>
             <Container>
                 <Menu.Item header as={NavLink} exact to='/home'>
                     <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
                     SysEgz
-        </Menu.Item>
-                <Menu.Item >
-                    <Dropdown pointing='top left' text="Kursy">
-                        <Dropdown.Menu>
-                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<CourseForm />)} />
-                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/courses' />
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Menu.Item >
-                <Menu.Item >
-                    <Dropdown pointing='top left' text="Zadania">
-                        <Dropdown.Menu>
-                            <Dropdown.Item text='Dodaj' icon='plus' />
-                            <Dropdown.Item text='Przeglądaj' icon='list' />
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Menu.Item>
-                <Menu.Item >
-                    <Dropdown pointing='top left' text="Grupy">
-                        <Dropdown.Menu>
-                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<GroupForm />)} />
-                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/groups' />
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Menu.Item>
-                <Menu.Item >
-                    <Dropdown pointing='top left' text="Użytkownicy">
-                        <Dropdown.Menu>
-                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<RegisterUserForm />)} />
-                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/users' />
-                        </Dropdown.Menu>
-                    </Dropdown>
                 </Menu.Item>
                 {user && (
                     <Fragment>
+                        <NavBarAdmininstrator user={user} />
+                        <NavBarMainLecturer user={user} />
+                        <NavBarLecturer user={user} />
+                        <NavBarStudent user={user} />
                         <Menu.Item position='right'>
-                            <MenuHeader>{user.role}</MenuHeader>
+                            <MenuHeader>
+                                {user.role}
+                            </MenuHeader>
                         </Menu.Item>
                         <Menu.Item position='right'>
                             <Dropdown pointing='top left' text={user.userName}>
---
+++ src/features/nav/navroles/NavBarAdministrator.tsx
@@ -0,0 +1,59 @@
+import React, { useContext, Fragment } from 'react';
+import { Menu, Dropdown } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
+import { RootStoreContext } from '../../../app/stores/rootStore';
+import CourseForm from '../../course/CourseForm';
+import GroupForm from '../../group/GroupForm';
+import RegisterUserForm from '../../user/RegisterUserForm';
+import { ADMINISTRATOR_ROLE } from '../../../app/common/roles/roles';
+import { IUser } from '../../../app/models/user';
+
+interface IProps {
+    user: IUser;
+}
+
+const NavBarAdministrator: React.FC<IProps> = ({ user }) => {
+    const rootStore = useContext(RootStoreContext);
+    const { openModal } = rootStore.modalStore
+    if (user.role == ADMINISTRATOR_ROLE) {
+        return (
+            <Fragment>
+                <Menu.Item >
+                    <Dropdown pointing='top left' text="Kursy">
+                        <Dropdown.Menu>
+                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<CourseForm />)} />
+                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/courses' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item >
+                <Menu.Item >
+                    <Dropdown pointing='top left' text="Zadania">
+                        <Dropdown.Menu>
+                            <Dropdown.Item text='Dodaj' icon='plus' />
+                            <Dropdown.Item text='Przeglądaj' icon='list' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+                <Menu.Item >
+                    <Dropdown pointing='top left' text="Grupy">
+                        <Dropdown.Menu>
+                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<GroupForm />)} />
+                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/groups' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+                <Menu.Item >
+                    <Dropdown pointing='top left' text="Użytkownicy">
+                        <Dropdown.Menu>
+                            <Dropdown.Item text='Dodaj' icon='plus' onClick={() => openModal(<RegisterUserForm />)} />
+                            <Dropdown.Item text='Przeglądaj' icon='list' as={NavLink} exact to='/users' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+            </Fragment>
+        )
+    } else
+        return (<Fragment></Fragment>);
+}
+export default observer(NavBarAdministrator);
