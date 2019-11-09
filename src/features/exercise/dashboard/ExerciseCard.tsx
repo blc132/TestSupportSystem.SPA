d6667fda7d70de29e7eb74d575b674b6e6818c71
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-11-09 22:35:17 +0100

    Różne wyświetlanie ExerciseCard w zależności od roli aktualnie zalogowanego użytkownika

--- src/features/exercise/dashboard/ExerciseCard.tsx
+++ src/features/exercise/dashboard/ExerciseCard.tsx
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Card, Divider } from 'semantic-ui-react';
 import { IExercise } from '../../../app/models/exercise'
+import { Link } from 'react-router-dom';
+import { RootStoreContext } from '../../../app/stores/rootStore';
+import { ADMINISTRATOR_ROLE, LECTURER_ROLE, MAINLECTURER_ROLE, STUDENT_ROLE } from '../../../app/common/roles/roles';
 
 interface IProps {
     exercise: IExercise
 }
 
 const ExerciseCard: React.FC<IProps> = ({ exercise }) => {
+
+    const rootStore = useContext(RootStoreContext);
+    const { user } = rootStore.userStore;
+
     return (
         <Fragment>
             <Card>
                 <Card.Content>
-                    <Card.Header>{exercise.name}</Card.Header>
+                    {user && user.role === STUDENT_ROLE && (
+                        <Card.Header>{exercise.name}</Card.Header>
+                    )}
+                    {user && (user.role === ADMINISTRATOR_ROLE || user.role === LECTURER_ROLE || user.role === MAINLECTURER_ROLE) && (
+                        <Card.Header as={Link} to={`/exercise/${exercise.id}`}>{exercise.name}</Card.Header>
+                    )}
                     <Card.Meta>{exercise.author.firstName + " " + exercise.author.lastName}</Card.Meta>
                     <Card.Meta>{exercise.author.email}</Card.Meta>
                 </Card.Content>
